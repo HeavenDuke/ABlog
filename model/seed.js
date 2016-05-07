8549477136f860269e2a7f2b03142b2fce36fab9
HeavenDuke <liuhancheng@shangxiane.com>
2016-05-08 01:18:10 +0800

    完成初始化脚本

--- model/seed.js
+++ model/seed.js
@@ -4,23 +4,16 @@
 
 var database = require('../model').loader;
 
-
-
-(function () {
-    (function connectDb(){
-        require('mongoose').connect(require('../config/config')().mongodb, {
-            server: {
-                poolSize: 12,
-                socketOptions: {
-                    keepAlive: 1
-                }
-            }
-        });
-    })();
+var init = function () {
+    var mongoose = require('mongoose');
+    mongoose.connect(require('../config/config')().mongodb);
     var User = database.models.user;
     var user = new User();
     user.username = "HeavenDuke";
     user.password = "md5$11111195$378BF8EB624C64A198A00C299B82CF41";
     user.save();
     console.log('finish deploying database seed.');
-})();
+    mongoose.disconnect();
+};
+
+init();

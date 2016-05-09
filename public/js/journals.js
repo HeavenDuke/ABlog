08ffc253d8c56dac6cf64dee2ed04561db0267de
HeavenDuke <liuhancheng@shangxiane.com>
2016-05-09 15:08:20 +0800

    添加markdown编辑框的预览

--- public/js/journals.js
+++ public/js/journals.js
@@ -9,9 +9,25 @@
         journal_content_container.html(journal_content_container.text());
     };
 
+    var prepare_journal_previewer = function () {
+        var journal_previewer_entry = $("#journal_previewer_entry");
+        journal_previewer_entry.on("click", function () {
+            var journal_previewer = $("#journal_previewer");
+            var journal_previewer_content = $("#journal_previewer_content");
+            var journal_raw_content = markdown.toHTML($("textarea[name='content']").val(), "Maruku");
+            journal_previewer_content.html(journal_raw_content);
+        });
+    }
+
     $(document).on('ready', function () {
-        console.log(window.location.href.match(/\/journals/));
-        if (window.location.href.match(/\/journals\/\w*/) != null) {
+        if (window.location.href.match(/\/journals\/\w+\/edit/) != null) {
+            prepare_journal_previewer();
+        }
+        else if (window.location.href.match(/\/journals\/new/) != null) {
+            console.log(2222)
+            prepare_journal_previewer();
+        }
+        else if (window.location.href.match(/\/journals\/\w+/) != null) {
             prepare_journal_detail();
         }
     });

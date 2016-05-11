f5cfe8dbd4b15573092d25359cf4377d64099395
DoubleDeckers <seer_buaasoft2015@163.com>
2016-05-12 03:04:23 +0800

    修正了日记的排序错误

--- controller/diaries/index.js
+++ controller/diaries/index.js
@@ -4,8 +4,7 @@
 
 exports.index = function *() {
     var Diary = global.database.models.diary;
-    console.log(this.session)
-    var diaries = yield Diary.find({}).sort({recorded_at: -1});
+    var diaries = yield Diary.find({}).sort({recorded_date: -1});
     if (!this.request.query.remote) {
         this.render('diaries/index', {title: "每日小记", current_user: this.session.user, diaries: diaries, current_module: this.current_module});
     }

3423d6b40e88bfa20143d476b6fb825655955d26
DoubleDeckers <seer_buaasoft2015@163.com>
2016-05-17 01:33:12 +0800

    修正日记排序

--- controller/diaries/index.js
+++ controller/diaries/index.js
@@ -14,7 +14,7 @@ function get_thumb_path(image_path) {
 
 exports.index = function *() {
     var Diary = global.database.models.diary;
-    var diaries = yield Diary.find({}).sort({recorded_date: -1, recorded_at: 1});
+    var diaries = yield Diary.find({}).sort({recorded_date: -1, recorded_at: -1});
     if (!this.request.query.remote) {
         this.render('diaries/index', {title: "每日小记", current_user: this.session.user, diaries: diaries, Diary: Diary, current_module: this.current_module, mood_list: Diary.mood_list(), tag_list: Diary.tag_list()});
     }

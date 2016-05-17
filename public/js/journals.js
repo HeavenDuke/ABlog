36af3fd5aaf0034cd28db88fb6605819148a0351
DoubleDeckers <seer_buaasoft2015@163.com>
2016-05-17 23:20:00 +0800

    调整一个小细节

--- public/js/journals.js
+++ public/js/journals.js
@@ -23,6 +23,11 @@
 
         $("a[id$='_reply_entry']").on('click', function () {
             var entry_id = $(this).attr('id');
+            var toggle_hash = {
+                "回复": "收起",
+                "收起": "回复"
+            }
+            $(this).text(toggle_hash[$(this).text().trim()]);
             var comment_id = entry_id.substring(0, entry_id.indexOf("_reply_entry"));
             $("#" + comment_id + "_reply_container").slideToggle();
         });

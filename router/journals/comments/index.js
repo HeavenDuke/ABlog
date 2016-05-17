800f84e8d1e00ca5ea651a5eb9610ffb66e7649b
DoubleDeckers <seer_buaasoft2015@163.com>
2016-05-17 22:48:14 +0800

    实现评论回复的基本功能，等待调整样式

--- router/journals/comments/index.js
+++ router/journals/comments/index.js
@@ -5,11 +5,14 @@
 var commentsController = require('../../../controller').journal.comments;
 var authentication = require('../../../middlewares/authentication');
 var visit_recorder = require('../../../middlewares/visit_recorder');
+var repliesRouter = require('./replies');
 
 module.exports = function(app) {
 
     app.post('comment-create', '/journals/:journal_id/comments', visit_recorder, commentsController.create);
 
     app.del('comment-destroy', '/journals/:journal_id/comments/:comment_id', visit_recorder, authentication, commentsController.destroy);
+
+    repliesRouter(app);
 
 };

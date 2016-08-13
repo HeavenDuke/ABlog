d11444580a15f162d2fa6a86b3c9f3a42afafb59
HeavenDuke <liuhancheng@shangxiane.com>
2016-08-13 15:57:52 +0800

    搭好了相册模块的架子

--- controller/galleries/index.js
+++ controller/galleries/index.js
@@ -3,7 +3,14 @@
  */
 
 var index = function *(next) {
-
+    this.render('./galleries/index', {
+        title: "相册列表",
+        current_guest: this.session.guest,
+        current_user: this.session.user,
+        galleries: [],
+        current_module: this.current_module,
+        redirect_url: this.request.url
+    });
 };
 
 var show = function *(next) {
--- router/galleries/index.js
+++ router/galleries/index.js
@@ -1,7 +1,27 @@
 /**
  * Created by heavenduke on 16-8-13.
  */
+var galleries_controller = require('../../controller').galleries;
+var authentication = require('../../middlewares/authentication');
+var set_redirection = require('../../middlewares/set_redirection');
+var visit_recorder = require('../../middlewares/visit_recorder');
 
-module.exports = function (app) {
-    
+module.exports = function(app) {
+
+    var current_module = function *(next) {
+        this.current_module = "gallery";
+        yield next;
+    };
+
+    app.get('galleries-index', '/galleries', visit_recorder, set_redirection, current_module, galleries_controller.index);
+
+    app.get('galleries-show', '/galleries/:gallery_id', visit_recorder, set_redirection, current_module, galleries_controller.show);
+
+    app.post('galleries-create', '/galleries', visit_recorder, authentication.admin_only, set_redirection, current_module, galleries_controller.create);
+
+    app.put('galleries-update', '/galleries/:gallery_id', visit_recorder, authentication.admin_only, set_redirection, current_module, galleries_controller.update);
+
+    app.delete('galleries-destroy', '/galleries', visit_recorder, authentication.admin_only, set_redirection, current_module, galleries_controller.destroy);
+
+
 };

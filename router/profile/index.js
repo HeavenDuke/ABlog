3d6a90bbb286c5c426161468f02a259f41e2390e
HeavenDuke <trashlhc@163.com>
2017-02-17 00:39:26 +0800

    调整编辑简历页面的样式

--- router/profile/index.js
+++ router/profile/index.js
@@ -9,7 +9,7 @@ let authentication = require('../../middlewares/authentication').admin_only;
 
 module.exports = function (app) {
 
-    app.get('profile-detail', "/profile", visit_recorder, set_redirection, authentication, profile_controller.show);
+    app.get('profile-detail', "/profile", visit_recorder, set_redirection, profile_controller.show);
 
     app.get('profile-edit', "/profile/edit", visit_recorder, set_redirection, authentication, profile_controller.edit);
 
